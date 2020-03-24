7c03a9e4d761a45ae509b8b055e47ead47ed540f
Ben Weinberg <weinbergben33@gmail.com>
2020-03-23 21:50:35 -0400

    finished pending screen

--- lib/Widgets/pending_transaction.dart
+++ lib/Widgets/pending_transaction.dart
@@ -1,9 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class PendingTransaction extends StatelessWidget {
   static const routeName = 'pending-transaction';
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.send),
+        title: Text('Pending Transaction'),
+      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Your transaction is being processed',
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 35),
+              SpinKitChasingDots(
+                color: Colors.blue,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
